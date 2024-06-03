baf015f90611a4422bda40b969e0568ef1f7b55e
phuong23401 <hoaphuong23401@gmail.com>
2024-06-03 22:05:25 +0700

    update code

---
+++ uno.config.ts
@@ -0,0 +1,31 @@
+import {
+  defineConfig,
+  presetAttributify,
+  presetIcons,
+  presetTypography,
+  presetUno,
+  presetWebFonts,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss'
+
+export default defineConfig({
+  theme: {
+    colors: {
+      darknavi: '#0c0c4f',
+    },
+  },
+  presets: [
+    presetUno(),
+    presetAttributify(),
+    presetIcons(),
+    presetTypography(),
+    presetWebFonts({
+      fonts: {
+        provider: 'google',
+        inter: 'Inter',
+      },
+    }),
+  ],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
+})
