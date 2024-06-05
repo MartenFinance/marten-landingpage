7d34605c3f32c121229d389878be0a556ccbf276
phuong23401 <hoaphuong23401@gmail.com>
2024-06-05 17:13:10 +0700

    update config css

--- uno.config.ts
+++ uno.config.ts
@@ -14,6 +14,28 @@ export default defineConfig({
     colors: {
       darknavi: '#0c0c4f',
     },
+    container: {
+      screens: {
+        sm: '425px',
+        sm1: '576px',
+        md: '768px',
+        lg: '992px',
+        lg1: '1200px',
+        xl: '1440px',
+        '2xl': '1920px',
+        '3xl': '2560px',
+      },
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1.75rem',
+        sm1: '2.5rem',
+        lg: '3rem',
+        xl: '6rem',
+        '2xl': '11rem',
+        '3xl': '31rem',
+      },
+    },
   },
   presets: [
     presetUno(),
