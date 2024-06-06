a42f5eaa6cf2dd3ac36acd9901089429972c3950
phuong23401 <hoaphuong23401@gmail.com>
2024-06-06 18:40:58 +0700

    update section title

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -6,7 +6,7 @@ function SectionTitle(props: ITitleProps): JSX.Element {
   const { children } = props
 
   return (
-    <h1 className="font-archiaregular text-[46px] leading-[55px] relative mb-0 font-bold text-center">
+    <h1 className="font-archiaregular text-[46px] leading-[55px] relative m-0 font-bold text-center">
       {children}
     </h1>
   )
