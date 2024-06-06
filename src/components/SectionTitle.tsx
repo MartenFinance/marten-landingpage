302b7ce4ef07bc1836e1aa916971c8e48ccb34b5
phuong23401 <hoaphuong23401@gmail.com>
2024-06-06 13:30:39 +0700

    add section title

---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,15 @@
+interface ITitleProps {
+  children: React.ReactNode
+}
+
+function SectionTitle(props: ITitleProps): JSX.Element {
+  const { children } = props
+
+  return (
+    <h1 className="font-archiaregular text-[46px] leading-[55px] relative mb-0 font-bold text-center">
+      {children}
+    </h1>
+  )
+}
+
+export default SectionTitle
