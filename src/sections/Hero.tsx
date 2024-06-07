3f47d304a39c1d839254904da16805bd665f4eb7
phuong23401 <hoaphuong23401@gmail.com>
2024-06-07 11:25:49 +0700

    update hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,17 @@
 import Button from '../components/Button'
+import { FaChartSimple } from 'react-icons/fa6'
+import { FaPercent } from 'react-icons/fa'
+import { PiCurrencyDollarFill } from 'react-icons/pi'
 
 function Hero(): JSX.Element {
+  const contentTitle =
+    'text-[#16181f] text-[28px] font-archiaregular mt-0 mb-8'
+  const contentText = 'text-[#16181f] font-archiaregular m-0'
+
   return (
     <div className="relative w-full mx-auto py-[120px]">
       <div className="container">
-        <div className="mx-auto lg:w-1/2 max-w-screen-2xl">
+        <div className="mx-auto max-w-screen-2xl">
           <div className="text-center">
             <p className="text-white text-[64px] font-archiaregular mt-0 mb-10">
               0% interest loans
@@ -18,8 +25,32 @@ function Hero(): JSX.Element {
             <Button>Borrow now</Button>
             <Button type="secondary">Learn more</Button>
           </div>
-
-          <div></div>
+        </div>
+      </div>
+      <div className="bg-white flex flex-col items-center justify-center gap-[30px] mt-[50px] p-[3rem] xl:flex-row">
+        <div className="text-left max-w-[400px]">
+          <FaChartSimple className="text-[#16181f] text-[36px]" />
+          <p className={contentTitle}>0% Interest Rate</p>
+          <p className={contentText}>
+            Marten charges a small, one-time fee to borrow USDM instead of
+            highly variable interest rates.
+          </p>
+        </div>
+        <div className="text-left max-w-[400px]">
+          <FaPercent className="text-[#16181f] text-[36px]" />
+          <p className={contentTitle}>110% Collateral Ratio</p>
+          <p className={contentText}>
+            Marten's efficient liquidation mechanism allows users to get the
+            most liquidity for their ETH
+          </p>
+        </div>
+        <div className="text-left max-w-[400px]">
+          <PiCurrencyDollarFill className="text-[#16181f] text-[36px]" />
+          <p className={contentTitle}>Unstoppable Stablecoin</p>
+          <p className={contentText}>
+            USDM is an immutable, decentralized stablecoin with no centralized
+            counterparty risk.
+          </p>
         </div>
       </div>
     </div>
